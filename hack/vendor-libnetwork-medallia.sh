aca3488b9d35ad7baf0dec54b2f6a3fab2737b10
Diego Erdody <diego@medallia.com>
2016-03-07 13:40:06 -0800

    update vendoring

--- hack/vendor-libnetwork-medallia.sh
+++ hack/vendor-libnetwork-medallia.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$BASH_SOURCE")/.."
 source 'hack/.vendor-helpers.sh'
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork 5dd6d68508e5c4aad466f52e4725d63a575b2353 https://github.com/medallia/libnetwork.git
+clone git github.com/docker/libnetwork 2c871751462e8b01f784f203b876ea7b063e87e7 https://github.com/medallia/libnetwork.git
