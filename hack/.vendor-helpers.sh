89108a1ee4f0ba817fa48da2fd2f8f9641678156
axqx <axqx@users.noreply.github.com>
2017-02-20 19:15:26 -0300

    Point to RUNC version using a specific tag instead of a branch (#13)

--- hack/.vendor-helpers.sh
+++ hack/.vendor-helpers.sh
@@ -4,7 +4,7 @@ PROJECT=github.com/docker/docker
 # Downloads dependencies into vendor/ directory
 mkdir -p vendor
 
-MEDALLIA_RUNC_VERSION=medallia-1.12.6-integration
+MEDALLIA_RUNC_VERSION=1.12.6-medallia-2
 MEDALLIA_LIBNETWORK_VERSION=medallia-1.12.6-integration
 if ! go list github.com/docker/docker/docker &> /dev/null; then
 	rm -rf .gopath
